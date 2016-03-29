1c09a28ed8642353ff115ea2f1b870bd5b46f413
Pradeepiit <pradeeprengaswamy@gmail.com>
2016-03-29 10:55:00 +0530

    removed the unwanted commented code

--- validateInversion.m
+++ validateInversion.m
@@ -1,20 +1,5 @@
 function flag = validateInversion(bpfSSS)
 
-% flag = 1;
-% peaks = findpeaks(bpfSSS);
-% peaks = peaks(peaks > 0);
-% l1 = sum(abs(diff(peaks)));
-% 
-% peaks = [];
-% bpfSSS = bpfSSS .*-1;
-% peaks = findpeaks(bpfSSS);
-% peaks = peaks(peaks > 0);
-% l2 = sum(abs(diff(peaks)));
-% 
-% if(l1 < l2)
-%     flag = 0;
-% end
-
 flag = 1;
 [peaks,ind] = findpeaks(bpfSSS,'MinPeakHeight',max(bpfSSS)/6);
 l1 = length(ind);
